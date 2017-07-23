0fd037a5d2e8dcdf7d48b4b2c7824f40227f60e5
DeliaCodes <deliacodes@gmail.com>
2017-07-23 16:49:45 -0700

    prepped app and html file - ready to create app

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // your code here!
+
+//first grab the form input
+
+
+//then process it. Use the copy for the next three functions.
+//methods: .toLowerCase,.split,.trim,
+
+//total word count ie array.length+1=total
+//output to 
+
+//Unique word count -
+//sort array. loop over the array looking for if ___ compares
+//output to
+
+//Average word length in characters of the submitted text
+//loop over array var count+=str.length
+//output to
+
+//remove hidden class
+
+
