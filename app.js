afd7528f420940e70a1ac58663a089cb3efa54ac
DeliaCodes <deliacodes@gmail.com>
2017-07-23 19:22:37 -0700

    creating part 1

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 // your code here!
 
-//first grab the form input
+//first grab the form input. 
+//Q: Do I need to put this inside of a function?
+var text;
 
+$('#form-text').submit{ function (input) {
 
-//then process it. Use the copy for the next three functions.
+	input = $('#user-text').val();
+	//then process it. Use the copy for the next three functions.
 //methods: .toLowerCase,.split,.trim,
+text= input.toLowerCase().split().trim()
+return text;
+}
+};
+
+
+
 
 //total word count ie array.length+1=total
-//output to 
+//output to #js-count with ? .text
+$('#js-count').text();
+
 
 //Unique word count -
 //sort array. loop over the array looking for if ___ compares
 //output to
+text.sort();
+$('#js-unique').text();
 
 //Average word length in characters of the submitted text
 //loop over array var count+=str.length
 //output to
+$('#js-average').text();
 
 //remove hidden class
+
+$('dl').removeClass('hidden');
 
 
